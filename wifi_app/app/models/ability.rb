b160fa98da139f4fe108bfea7121915a9cd03425
shehryarriaz <shehryarriaz@gmail.com>
2014-09-21 17:06:56 +0100

    Updating ability.rb for User permissions to use CRUD for only their networks.

--- wifi_app/app/models/ability.rb
+++ wifi_app/app/models/ability.rb
@@ -6,10 +6,13 @@ class Ability
     if user.role? :admin
         can :manage, :all
     elsif user.role? :basic_user
-        can :read, Network
+        can :read, Network, user_id: user.id
         can :create, Network
+        can :update, Network, user_id: user.id
+        can :destroy, Network, user_id: user.id
         can :read, Friendship
-        can :create, Friendship
+        can :create, Friendship, user_id: user.id
+        can :destroy, Friendship
     else
         can :create, User
     end
