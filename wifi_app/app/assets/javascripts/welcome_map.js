2f7337021fb8268b1162dbe767eb6845ae5c0cb2
shehryarriaz <shehryarriaz@gmail.com>
2014-09-22 17:21:19 +0100

    Updating welcome page Google Maps API to focus on user location by default.

--- wifi_app/app/assets/javascripts/welcome_map.js
+++ wifi_app/app/assets/javascripts/welcome_map.js
@@ -4,19 +4,39 @@ welcomeMap.initialize = function() {
   var mapCanvas = $('#map-canvas')[0];
   if (!!mapCanvas){
 
-    // if (navigator.geolocation) {
-    //   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    // } else {
-    //   alert('Your browser does not support geolocation.');
-    // }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    } else {
+      alert('Your browser does not support geolocation.');
+    }
 
-    var mapOptions = {
-      center: { lat:  51.52, lng: -0.115 },
-      zoom: 14,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+    function successCallback(position) {
+      console.log(position);
 
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+      var latlong = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      console.log(latlong);
+
+      var mapOptions = {
+        center: { lat:  position.coords.latitude, lng: position.coords.longitude },
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      var map = new google.maps.Map(mapCanvas, mapOptions);
+    }
+
+    function errorCallback(error) {
+      console.log(error);
+    }
+
+    // var mapOptions = {
+    //   center: { lat:  51.52, lng: -0.115 },
+    //   zoom: 14,
+    //   mapTypeId: google.maps.MapTypeId.ROADMAP
+    // };
+
+    // var map = new google.maps.Map(mapCanvas, mapOptions);
   }
 }
 
