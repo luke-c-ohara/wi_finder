4d1916d75f653b993f35b6cea909aadc1ab09091
shehryarriaz <shehryarriaz@gmail.com>
2014-09-23 14:20:52 +0100

    Initial commit of multiple markers on welcome map.

--- wifi_app/app/assets/javascripts/welcome_map.js
+++ wifi_app/app/assets/javascripts/welcome_map.js
@@ -11,6 +11,8 @@ welcomeMap.initialize = function() {
     }
 
     function successCallback(position) {
+      var locations = [['You are here!', position.coords.latitude, position.coords.longitude], ['20 Eyre Street Hill', 51.5225220, -0.1103600]];
+
       var mapOptions = {
         center: { lat:  position.coords.latitude, lng: position.coords.longitude },
         zoom: 15,
@@ -19,36 +21,47 @@ welcomeMap.initialize = function() {
 
       var map = new google.maps.Map(mapCanvas, mapOptions);
 
-      var markerOptions = {
-        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-        map: map
+      var infoWindow = new google.maps.InfoWindow();
+
+      var marker, increment;
+
+      for (increment = 0; increment < locations.length; increment++) {
+        marker = new
       }
-
-      var marker = new google.maps.Marker(markerOptions);
-
-      var infoWindowOptions = {
-        content: 'You are here!'
-      };
-
-      var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-
-      infoWindow.open(map, marker);
-      google.maps.event.addListener(marker, 'click', function(){
-        infoWindow.open(map, marker);
-      });
     }
 
-    function errorCallback(error) {
-      console.log(error);
+    // OLD GEOLOCATOR WORKING
+    // function successCallback(position) {
+    //   var mapOptions = {
+    //     center: { lat:  position.coords.latitude, lng: position.coords.longitude },
+    //     zoom: 15,
+    //     mapTypeId: google.maps.MapTypeId.ROADMAP
+    //   };
+
+    //   var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    //   var markerOptions = {
+    //     position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+    //     map: map
+    //   }
+
+    //   var marker = new google.maps.Marker(markerOptions);
+
+    //   var infoWindowOptions = {
+    //     content: 'You are here!'
+    //   };
+
+    //   var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+    //   infoWindow.open(map, marker);
+    //   google.maps.event.addListener(marker, 'click', function(){
+    //     infoWindow.open(map, marker);
+    //   });
+    // }
+
+    // function errorCallback(error) {
+    //   console.log(error);
     }
-
-    // var mapOptions = {
-    //   center: { lat:  51.52, lng: -0.115 },
-    //   zoom: 14,
-    //   mapTypeId: google.maps.MapTypeId.ROADMAP
-    // };
-
-    // var map = new google.maps.Map(mapCanvas, mapOptions);
   }
 }
 
