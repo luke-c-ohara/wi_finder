cc0711bf81c450873cd2a88ee7911246dbe1dd17
shehryarriaz <shehryarriaz@gmail.com>
2014-09-24 20:23:29 +0100

    Adding Foundation js files. Foundation, topbar and dropdown.

--- wifi_app/app/assets/javascripts/application.js
+++ wifi_app/app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation.js
+//= require foundation.topbar.js
+//= require foundation.dropdown.js
 //= require welcome_map.js
 //= require_tree .
 
